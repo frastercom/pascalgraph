76c547ab2650da5db8070babb5b90f47159b60f7
frastercom <37472618+frastercom@users.noreply.github.com>
2022-02-20 10:30:10 +0300

    Update pascaltest.pas
    Убрали мерцание и нарисавали человечка

--- pascaltest.pas
+++ pascaltest.pas
@@ -19,15 +19,33 @@ procedure sharic;
 begin
   setbrushcolor(clBrown);
   circle(x,y,20);
+  line(x,y+20,x,y+20+100);
+  line(x,y+20+10,x-50,y+20+50);
+  line(x,y+20+10,x+50,y+20+50);
+  line(x,y+20+100,x-50,y+20+100+50);
+  line(x,y+20+100,x+50,y+20+100+50);
+end;
+
+procedure sharic1;
+begin
+  setbrushcolor(clBrown);
+  circle(x,y,20);
+  line(x,y+20,x,y+20+100);
+  line(x,y+20+10,x-50,y+20+50);
+  line(x,y+20+10,x+50,y+20+50);
+  line(x,y+20+100,x-50,y+20+100+50);
+  line(x,y+20+100,x+0,y+20+100+50);
 end;
  
 begin
   f:=true;
   x:=windowwidth div 2;
   y:=windowheight div 2;
+  lockdrawing; //убираем мерцание 
   repeat
     clearwindow;
     onkeydown:=klavisha;
-    sharic;
+    sharic1;
+    redraw; //убираем мерцание
   until not f;
 end.
