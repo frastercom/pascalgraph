6158f859d7fc4aac2bc9da5b92246469a3b338e2
frastercom <37472618+frastercom@users.noreply.github.com>
2022-03-06 09:53:13 +0300

    new version

--- pascaltest.pas
+++ pascaltest.pas
@@ -1,6 +1,52 @@
 uses graphabc;
+const max=10;
+speed = 10;
+
 var x,y:integer;
     f:boolean;
+    a:array[1..max,1..4] of integer;//new coment
+    active:integer;
+    
+procedure picture(x:integer; y:integer);//отображение снаряда
+begin
+  circle(x,y,10);
+  FloodFill(x,y,clBlack);
+end;
+
+procedure arr();
+var variant:integer;
+begin
+  variant := 100;
+  if active < max then
+  begin
+    variant := random(100);
+    if variant<10 then
+      for var i:=1 to max do
+        if a[i,1]=0 then
+        begin
+          a[i,1]:=1;
+          a[i,2]:=WindowWidth+10;
+          a[i,3]:=random(WindowHeight);
+          a[i,4]:=random(speed)+1;
+          active:=active+1;
+          break;
+        end;
+  end;
+  for var i:=1 to max do
+  begin
+    if a[i,1]=1 then
+    begin
+      picture(a[i,2],a[i,3]);
+      a[i,2]:=a[i,2]-a[i,4];
+      if (a[i,2]<1) then
+      begin
+        a[i,1]:=0;
+        active:=active-1;
+      end;
+    end;
+  end;
+end;
+    
 procedure klavisha(key:integer);
 begin
   case key of
@@ -46,6 +92,8 @@ begin
     clearwindow;
     onkeydown:=klavisha;
     sharic1;
+    arr();
+    sleep(1);
     redraw; //убираем мерцание
   until not f;
 end.
